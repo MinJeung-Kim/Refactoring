7aec79660809d205b0f4bcfa55a55026d4156d19
MinJeung-Kim <focso5@gmail.com>
2023-09-06 12:48:12 +0900

    Fix: 6-8 - 매개변수

--- ch6/6-8.js
+++ ch6/6-8.js
@@ -30,12 +30,12 @@ const station = {
     { temp: 53, time: "2016-11-10 09:20" },
     { temp: 58, time: "2016-11-10 09:30" },
     { temp: 53, time: "2016-11-10 09:40" },
-    { temp: 51, time: "2016-11-10 09:50" },
+    { temp: 51, time: "2016-11-10 09:50" }, 
   ],
 };
 
 const operationPlan = new NumberRange(51, 53);
 
-const result = readingsOutsideRange(operationPlan);
+const result = readingsOutsideRange(station, operationPlan);
 
 console.log(result);
