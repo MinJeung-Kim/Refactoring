b40c1c815b07d631d8fb8e2702ffd65157b02611
MinJeung-Kim <focso5@gmail.com>
2023-09-25 12:23:31 +0900

    Fix: 8-8

--- ch8/8-8.js
+++ ch8/8-8.js
@@ -1,19 +1,12 @@
 export function acquireData(input) {
-  const lines = input.split('\n');
-  let firstLine = true;
-  const result = [];
-  for (const line of lines) {
-    if (firstLine) {
-      firstLine = false;
-      continue;
-    }
-    if (line.trim() === '') continue;
-    const record = line.split(',');
-    if (record[1].trim() === 'India') {
-      result.push({ city: record[0].trim(), phone: record[2].trim() });
-    }
-  }
-  return result;
+  return input
+    .split("\n") // [ 'office, country, telephone', '', 'Chicago, USA, +1 312 373 1000', '', ... ]
+    .splice(1) // 제목 제외('office, country, telephone')
+    .filter((line) => line.trim() !== "") // 빈문자열 제외
+    .map((line) => line.split(",")) // [ [ 'Chicago', ' USA', ' +1 312 373 1000' ], [ 'Beijing', ' China', ' +86 4008 900 505' ], ... ]
+    .filter((line) => line[1].trim() === "India") // [ [ 'Bangalore', ' India', ' +91 80 4064 9570' ], [ 'Chennai', ' India', ' +91 44 660 44766' ] ]
+    .map((line) => ({ city: line[0].trim(), phone: line[2].trim() }));
+  // [ { city: 'Bangalore', phone: '+91 80 4064 9570' }, { city: 'Chennai', phone: '+91 44 660 44766' } ]
 }
 
 const input = `office, country, telephone\n
